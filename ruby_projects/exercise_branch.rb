c3dd2bf633f8320800de243d3d91ee35c8b2b97c
masa5281 <masa.h.5281@gmail.com>
2024-12-21 06:23:55 +0000

    Add: if演習用のファイルを追加

---
+++ ruby_projects/exercise_branch.rb
@@ -0,0 +1,12 @@
+# ↓演習で余計な記述をした↓
+# elsif age >= 6 && age < 12
+
+age = 11
+
+if age >= 12
+    puts '5,000'
+elsif age >= 6
+    puts '2,500'
+else
+    puts '1,000'
+end
---
+++ ruby_projects/if_animal.rb
@@ -0,0 +1,11 @@
+animal = 'dog'
+
+if animal == 'cat'
+    puts 'meow'
+elsif animal == 'dog'
+    puts 'bowwow'
+elsif animal == 'cow'
+    puts 'moomoo'
+else
+    puts 'Not found'
+end
