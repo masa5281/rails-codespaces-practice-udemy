5bebe978765596031a1147c02665b09adc8f43c4
masa5281 <masa.h.5281@gmail.com>
2024-12-21 11:51:08 +0000

    Add: each文とfor文の練習用のファイルを作成

---
+++ ruby_projects/each.rb
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5]
+
+# numbers.each do |number|
+#   puts number
+# end
+
+# 違う書き方
+# numbers.each {|number|
+#   puts number
+# }
+
+# 1行にする
+# numbers.each { |number| puts number }
+
+for number in numbers do
+  puts number
+end
