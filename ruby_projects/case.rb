bc54b62114bf193da949ec6c9d5e45cde57a2be3
masa5281 <masa.h.5281@gmail.com>
2024-12-21 07:05:06 +0000

    Add: case練習用ファイルの追加

---
+++ ruby_projects/case.rb
@@ -0,0 +1,24 @@
+# stone = 'sapphire'
+# if stone == 'ruby'
+#   puts 'July'
+# elsif stone == 'peridot'
+#   puts 'August'
+# elsif stone == 'sapphire'
+#   puts 'September'
+# else
+#   puts 'Not Found.'
+# end
+
+#caseに置き換える
+stone = 'sapphire'
+
+case stone
+when 'ruby'
+  puts 'July'
+when 'peridot'
+  puts 'August'
+when 'sapphire'
+  puts 'September'
+else
+  puts 'Not Found.'
+end
